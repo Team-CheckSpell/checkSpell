85b5167e1e939612693b95281a9540622319d161
Hanna Lee <8annahxxl@gmail.com>
2024-08-08 18:50:37 +0900

    feat: hanspell 맞춤법 검사 api 함수 작성 (#3)

---
+++ api.js
@@ -0,0 +1,42 @@
+import hanspell from 'hanspell';
+
+async function checkSpell(sentence) {
+  const TIMEOUT_MS = 6000;
+  const lastToken = getLastToken(sentence);
+
+  return new Promise((resolve, reject) => {
+    hanspell.spellCheckByPNU(
+      lastToken,
+      TIMEOUT_MS,
+      (res) => resolve(returnRes(res)),
+      (err) => reject(returnErr(err))
+    );
+  });
+}
+
+function getLastToken(sentence) {
+  const tokens = sentence.trim().split(' ');
+  return tokens[tokens.length - 1];
+}
+
+function returnRes(res) {
+  if (res.length === 0) {
+    return {
+      token: '',
+      suggestions: [],
+    };
+  } else {
+    return {
+      token: res[0].token,
+      suggestions: res[0].suggestions,
+    };
+  }
+}
+
+function returnErr(err) {
+  return {
+    error: err,
+  };
+}
+
+export default checkSpell;
